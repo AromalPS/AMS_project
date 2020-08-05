f884bdc6a0859a571f6a51fb5cc86c5a5b703969
AromalPS <19gce27@meaec.edu.in>
2020-08-05 16:33:40 +0530

    update

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+        // Validate form data
+        $this->validate($request,
+            [
+                'email' => 'required|string|email',
+                'password' => 'required|string|min:8',
+            ]
+        );
+
+        // Attempt to login as admin
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            // successful redirect to intended route or admin dashboard
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        //  If unsuccessful redirect back to login page with email and remember fields
+        return redirect()->back()->withlogin($request->only('email', 'remember'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,35 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function (){
+    // Dashboard routes
+    Route::get('/', 'AdminContoller@index')->name('admin.dashboard');
+
+    // Login routes
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
+    // Logout routes
+    Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+    // Register routes
+    Route::get('/register', 'Auth\AdminRegisterController@showRegistrationForm')->name('admin.register');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
+});
+
+Route::prefix('teacher')->group(function (){
+    // Dashboard routes
+    Route::get('/', 'TeacherContoller@index')->name('teacher.dashboard');
+
+    // Login routes
+    Route::get('/login', 'Auth\TeacherLoginController@showLoginForm')->name('teacher.login');
+    Route::post('/login', 'Auth\TeacherLoginController@login')->name('teacher.login.submit');
+
+    // Logout routes
+    Route::post('/logout', 'Auth\TeacherLoginController@logout')->name('teacher.logout');
+
+    // Register routes
+    Route::get('/register', 'Auth\TeacherRegisterController@showRegistrationForm')->name('teacher.register');
+    Route::post('/register', 'Auth\TeacherRegisterController@register')->name('teacher.register.submit');
+});
