95790b2cd1876ff814a4ddf2deaa1dd065c7bc7f
AromalPS <19gce27@meaec.edu.in>
2020-08-17 22:30:30 +0530

    update

---
+++ database/migrations/2020_08_17_164150_create_assignments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->string('Assigned by');
+            $table->integer('Maximum mark');
+            $table->timestamp('Assigned on', 0);
+            $table->dateTime('last_date_of_submission');
+            $table->enum('assigned_class', ['CE', 'ME', 'CSE', 'EC']);
+            $table->enum('assigned_year', ['1', '2', '3', '4']);
+            $table->longText('Question');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
